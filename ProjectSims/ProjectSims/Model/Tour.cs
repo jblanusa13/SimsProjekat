781d0912b91fcf2db5da0f274f3f32a0ac5cbead
Jovan Sarac <jovan_bn@outlook.com>
2023-03-07 11:57:17 +0100

    Added model tour

---
+++ ProjectSims/ProjectSims/Model/Tour.cs
@@ -0,0 +1,35 @@
+﻿using ProjectSims.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSims.Model
+{
+    internal class Tour : ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Descrption { get; set; }
+        public string Language { get; set; }
+        public int MaxNumberGuests { get; set; }
+        public string KeyPoints { get; set; }
+        public List<DateTime> StartOfTheTour { get; set; }
+        public int Duration { get; set; }
+        public string Images { get; set; }
+
+
+        //Implements
+        public void FromCSV(string[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] ToCSV()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectSims/ProjectSims/Serializer/ISerializable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSims.Serializer
+{
+    public interface ISerializable
+    {   
+        string[] ToCSV();
+        void FromCSV(string[] values);
+        
+        
+    }
+}
