1b2173fbcc376e53a75bbd159bb73fb371e6f56a
Jovan Sarac <jovan_bn@outlook.com>
2023-03-11 14:29:49 +0100

    Added tour search

--- ProjectSims/ProjectSims/Controller/TourController.cs
+++ ProjectSims/ProjectSims/Controller/TourController.cs
@@ -44,5 +44,205 @@ namespace ProjectSims.Controller
             tours.Subscribe(observer);
         }
 
+        public List<Tour> SearchTours(String location, int duration, String language, int numberGuests)
+        {
+            List<Tour> tours = GetAllTours();
+            List<Tour> wantedTours = new List<Tour>();
+
+
+            if (location != "")
+            {
+                if (duration != -1 && language == "" && numberGuests != -1)    //1
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if ((tour.Location.ToLower()).Contains(location.ToLower()) && tour.MaxNumberGuests >= numberGuests)
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration != -1 && language == "" && numberGuests == -1)   //2
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if ((tour.Location.ToLower()).Contains(location.ToLower()) && tour.Duration >= duration)
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration != -1 && language != "" && numberGuests == -1)    //3
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if ((tour.Location.ToLower()).Contains(location.ToLower()) && tour.Duration >= duration
+                            && (tour.Language.ToLower()).Contains(language.ToLower()))
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration != -1 && language != "" && numberGuests != -1)      //4
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if ((tour.Location.ToLower()).Contains(location.ToLower()) && tour.Duration >= duration
+                            && (tour.Language.ToLower()).Contains(language.ToLower()) && tour.MaxNumberGuests >= numberGuests)
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration == -1 && language != "" && numberGuests != -1)      //5
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if ((tour.Location.ToLower()).Contains(location.ToLower()) &&
+                           (tour.Language.ToLower()).Contains(language.ToLower()) && tour.MaxNumberGuests >= numberGuests)
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration == -1 && language == "" && numberGuests != -1)      //6
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if ((tour.Location.ToLower()).Contains(location.ToLower()) && tour.MaxNumberGuests >= numberGuests)
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration == -1 && language != "" && numberGuests == -1)         //7
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if ((tour.Location.ToLower()).Contains(location.ToLower()) && (tour.Language.ToLower()).Contains(language.ToLower()))
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration == -1 && language == "" && numberGuests == -1)         //8
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if ((tour.Location.ToLower()).Contains(location.ToLower()))
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+            }
+            else if (location == "")
+            {
+                if (duration != -1 && language == "" && numberGuests != -1)    //9
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if (tour.MaxNumberGuests >= numberGuests && tour.Duration >= duration)
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration != -1 && language == "" && numberGuests == -1)   //10
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if (tour.Duration >= duration)
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration != -1 && language != "" && numberGuests == -1)    //11
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if (tour.Duration >= duration
+                            && (tour.Language.ToLower()).Contains(language.ToLower()))
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+                    return wantedTours;
+                }
+                else if (duration != -1 && language != "" && numberGuests != -1)      //12
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if (tour.Duration >= duration && (tour.Language.ToLower()).Contains(language.ToLower())
+                            && tour.MaxNumberGuests >= numberGuests)
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+                    return wantedTours;
+                }
+                else if (duration == -1 && language != "" && numberGuests != -1)      //13
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if ((tour.Language.ToLower()).Contains(language.ToLower()) && tour.MaxNumberGuests >= numberGuests)
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration == -1 && language == "" && numberGuests != -1)      //14
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if (tour.MaxNumberGuests >= numberGuests)
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+                else if (duration == -1 && language != "" && numberGuests == -1)         //15
+                {
+                    foreach (Tour tour in tours)
+                    {
+                        if ((tour.Language.ToLower()).Contains(language.ToLower()))
+                        {
+                            wantedTours.Add(tour);
+                        }
+                    }
+
+                    return wantedTours;
+                }
+            }
+
+            return wantedTours;
+
+        }
+
     }
 }
--- ProjectSims/ProjectSims/TourDisplayAndSearchView.xaml.cs
+++ ProjectSims/ProjectSims/TourDisplayAndSearchView.xaml.cs
@@ -66,5 +66,56 @@ namespace ProjectSims
             }
 
         }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            String location = LocationTextBox.Text;
+
+            int duration;
+            //Validation wrong input
+            if (string.IsNullOrEmpty(DurationTextBox.Text))
+            {
+                duration = -1;
+            }
+            else if( !int.TryParse(DurationTextBox.Text, out duration) )
+            {
+                MessageBox.Show("Wrong input! Duration tour must be a integer!");
+                return;
+            }
+
+            String language = LanguageTextBox.Text;
+
+            int numberGuests;
+            //Validation wrong input
+            if (string.IsNullOrEmpty(NumberGuestsTextBox.Text))
+            {
+                numberGuests = -1;
+            }
+            else if( !int.TryParse(NumberGuestsTextBox.Text, out numberGuests) )
+            {
+                MessageBox.Show("Wrong input! Number guests on tour must be a integer!");
+                return;
+            }
+
+            if (location == "" && duration == -1 && language == "" && numberGuests == -1)         //16. case (nothing entered)
+            {
+                MessageBox.Show("You must enter some information for search!");
+                ListTour.Clear();
+                foreach (var tour in tourController.GetAllTours())
+                {
+                    ListTour.Add(tour);
+                }
+                return;
+            }
+
+
+
+            List<Tour> wantedTours = tourController.SearchTours(location,duration,language,numberGuests);
+            ListTour.Clear();
+            foreach (Tour tour in wantedTours)
+            {
+                ListTour.Add(tour);
+            }
+        }
     }
 }
