c0ea7bd1c33d3a07fb8dada4505469b562e27e1a
Ana <anaranic01@gmail.com>
2023-03-14 12:26:23 +0100

    Data validation for tour creation

--- ProjectSims/ProjectSims/CreateTour.xaml.cs
+++ ProjectSims/ProjectSims/CreateTour.xaml.cs
@@ -19,13 +19,14 @@ using System.Xml.Linq;
 using ProjectSims.Model;
 using ProjectSims.Observer;
 using System.Media;
+using System.Text.RegularExpressions;
 
 namespace ProjectSims
 {
     /// <summary>
     /// Interaction logic for CreateTour.xaml
     /// </summary>
-    public partial class CreateTour : Window, INotifyPropertyChanged
+    public partial class CreateTour : Window, INotifyPropertyChanged, IDataErrorInfo
     {
         private readonly TourController _controller;
 
@@ -196,6 +197,77 @@ namespace ProjectSims
             }
         }
 
+        private Regex _durationRegex = new Regex("^([0-9]*\\.)?[0-9]+$");
+        private Regex _maxNumberGuestsRegex = new Regex("^[1-9][0-9]*$");
+        public String Error => null;
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName == "TourName")
+                {
+                    if (string.IsNullOrEmpty(TourName))
+                        return "Unesite naziv ture!";
+                }
+                else if (columnName == "Location")
+                {
+                    if (string.IsNullOrEmpty(Location))
+                        return "Unesite lokaciju!";
+                }
+                else if (columnName == "TourLanguage")
+                {
+                    if (string.IsNullOrEmpty(TourLanguage))
+                        return "Odaberite jezik!";
+                }
+                else if (columnName == "MaxNumberGuests")
+                {
+                    if (string.IsNullOrEmpty(MaxNumberGuests))
+                        return "Unesite broj mesta!";
+                    Match match = _maxNumberGuestsRegex.Match(MaxNumberGuests);
+                    if (!match.Success)
+                        return "Format nije ispravan!";
+                }
+                else if (columnName == "Duration")
+                {
+                    if (string.IsNullOrEmpty(Duration))
+                        return "Unesite trajanje ture!";
+                    Match match =_durationRegex.Match(Duration);
+                    if (!match.Success)
+                        return "Format nije ispravan!";
+                }
+                else if (columnName == "TourStart")
+                {
+                    if (string.IsNullOrEmpty(TourStart))
+                        return "Unesite datum i vreme početka ture!";
+                }
+                else if (columnName == "StartKeyPoint")
+                {
+                    if (string.IsNullOrEmpty(StartKeyPoint))
+                        return "Unesite početnu stanicu!";
+                }
+                else if (columnName == "FinishKeyPoint")
+                {
+                    if (string.IsNullOrEmpty(FinishKeyPoint))
+                        return "Unesite krajnju stanicu!";
+                }
+             return null;
+
+            }
+        }
+
+        private readonly string[] _validatedProperties = { "TourName", "Location", "TourLanguage", "MaxNumberGuests", "Duration", "TourStart", "StartKeyPoint", "FinishKeyPoint"};
+        public bool IsValid
+        {
+            get
+            {
+                foreach (var property in _validatedProperties)
+                {
+                    if (this[property] != null)
+                        return false;
+                }
+                return true;
+            }
+        }
         public CreateTour()
         {
             InitializeComponent();
@@ -213,8 +285,13 @@ namespace ProjectSims
 
         private void CreateTourClick(object sender, RoutedEventArgs e)
         {
-            _controller.Save(TourName, Location, Description, TourLanguage, MaxNumberGuests, StartKeyPoint,FinishKeyPoint,OtherKeyPoints,TourStart, Duration, Images);
-            Close();
+            if (IsValid)
+            {
+                _controller.Save(TourName, Location, Description, TourLanguage, MaxNumberGuests, StartKeyPoint, FinishKeyPoint, OtherKeyPoints, TourStart, Duration, Images);
+                Close();
+            }
+            else
+                MessageBox.Show("Nisu validno popunjena polja!");
             
         }
 
