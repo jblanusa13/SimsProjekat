412bdb33645bd12607580a55b0cef3faf3ba234a
Jelena <jblanusa13@gmail.com>
2023-03-15 14:44:04 +0100

    Added AccommodationReservation model, file handler and view

---
+++ ProjectSims/ProjectSims/Model/AccommodationReservation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectSims.Serializer;
+
+namespace ProjectSims.Model
+{
+    class AccommodationReservation : ISerializable
+    {
+        public int Id { get; set; }
+        public int AccommodationId { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly CheckOutDate { get; set; }
+
+        public AccommodationReservation() { }
+
+        public AccommodationReservation(int accommodationId, DateOnly checkInDate, DateOnly checkOutDate)
+        {
+            AccommodationId = accommodationId;
+            CheckInDate = checkInDate;
+            CheckOutDate = checkOutDate;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            AccommodationId = Convert.ToInt32(values[1]);
+            CheckInDate = DateOnly.Parse(values[2]);
+            CheckOutDate = DateOnly.Parse(values[3]);
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvvalues = { 
+                Id.ToString(), 
+                AccommodationId.ToString(),
+                CheckInDate.ToString(),
+                CheckOutDate.ToString()
+            };
+            return csvvalues;
+        }
+    }
+}
