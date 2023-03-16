b2521fbd9afe7ef6a385cac4e5a71ad8f2dfc79c
Jelena <jblanusa13@gmail.com>
2023-03-16 14:08:07 +0100

    Finished view for accommodation reservation

--- ProjectSims/ProjectSims/Model/AccommodationReservation.cs
+++ ProjectSims/ProjectSims/Model/AccommodationReservation.cs
@@ -13,14 +13,16 @@ namespace ProjectSims.Model
         public int AccommodationId { get; set; }
         public DateOnly CheckInDate { get; set; }
         public DateOnly CheckOutDate { get; set; }
+        public int GuestNumber { get; set; }
 
         public AccommodationReservation() { }
 
-        public AccommodationReservation(int accommodationId, DateOnly checkInDate, DateOnly checkOutDate)
+        public AccommodationReservation(int accommodationId, DateOnly checkInDate, DateOnly checkOutDate, int guestNumber)
         {
             AccommodationId = accommodationId;
             CheckInDate = checkInDate;
             CheckOutDate = checkOutDate;
+            GuestNumber = guestNumber;
         }
 
         public void FromCSV(string[] values)
@@ -29,6 +31,7 @@ namespace ProjectSims.Model
             AccommodationId = Convert.ToInt32(values[1]);
             CheckInDate = DateOnly.Parse(values[2]);
             CheckOutDate = DateOnly.Parse(values[3]);
+            GuestNumber = Convert.ToInt32(values[4]);
         }
 
         public string[] ToCSV()
@@ -37,7 +40,8 @@ namespace ProjectSims.Model
                 Id.ToString(), 
                 AccommodationId.ToString(),
                 CheckInDate.ToString(),
-                CheckOutDate.ToString()
+                CheckOutDate.ToString(),
+                GuestNumber.ToString()  
             };
             return csvvalues;
         }
--- ProjectSims/ProjectSims/View/AccommodationReservationView.xaml.cs
+++ ProjectSims/ProjectSims/View/AccommodationReservationView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,17 +13,118 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ProjectSims.Controller;
+using ProjectSims.Model;
+using ProjectSims.Observer;
 
 namespace ProjectSims.View
 {
     /// <summary>
     /// Interaction logic for AccommodationReservationView.xaml
     /// </summary>
-    public partial class AccommodationReservationView : Window
+    public partial class AccommodationReservationView : Window, INotifyPropertyChanged, IObserver
     {
-        public AccommodationReservationView()
+        private string _accommodationName;
+        public string AccommodationName
+        {
+            get => _accommodationName;
+            set
+            {
+                if (value != _accommodationName)
+                {
+                    _accommodationName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _location;
+        public string Location
+        {
+            get => _location;
+            set
+            {
+                if (value != _location)
+                {
+                    _location = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _type;
+        public string Type
+        {
+            get => _type;
+            set
+            {
+                if (value != _type)
+                {
+                    _type = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _maxGuests;
+        public string MaxGuests
+        {
+            get => _maxGuests;
+            set
+            {
+                if (value != _maxGuests)
+                {
+                    _maxGuests = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _minDays;
+        public string MinDays
+        {
+            get => _minDays;
+            set
+            {
+                if (value != _minDays)
+                {
+                    _minDays = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private AccommodationController _accommodationController;
+
+        public AccommodationReservationView(Accommodation SelectedAccommodation, AccommodationController accommodationController)
         {
             InitializeComponent();
+            DataContext = this;
+
+            AccommodationName = SelectedAccommodation.Name;
+            Location = SelectedAccommodation.Location;
+            Type = SelectedAccommodation.Type.ToString();
+            MaxGuests = SelectedAccommodation.GuestMaximum.ToString();
+            MinDays = SelectedAccommodation.MinimumReservationDays.ToString();
+
+            _accommodationController = accommodationController;
+            _accommodationController.Subscribe(this);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
