03e2574c928c2d8fa8334ac32902d5e9f3737495
Jovan Sarac <jovan_bn@outlook.com>
2023-04-10 12:42:10 +0200

    Create window for tour tracking guest2(Guest2TrackingTourView)

---
+++ ProjectSims/ProjectSims/WPF/View/Guest2View/Guest2TrackingTourView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectSims.WPF.View.Guest2View
+{
+    /// <summary>
+    /// Interaction logic for Guest2TrackingTourView.xaml
+    /// </summary>
+    public partial class Guest2TrackingTourView : Window
+    {
+        public Guest2TrackingTourView()
+        {
+            InitializeComponent();
+        }
+    }
+}
