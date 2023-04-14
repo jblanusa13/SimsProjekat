d475774d023a2aa8fac6542a0f17b27757fe8e49
Jelena <jblanusa13@gmail.com>
2023-04-14 09:54:41 +0200

    Finished view for rating accommodation and owner

---
+++ ProjectSims/ProjectSims/WPF/View/Guest1View/AccommodationRating.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectSims.WPF.View.Guest1View
+{
+    /// <summary>
+    /// Interaction logic for AccommodationRating.xaml
+    /// </summary>
+    public partial class AccommodationRating : Window
+    {
+        public AccommodationRating()
+        {
+            InitializeComponent();
+        }
+    }
+}
