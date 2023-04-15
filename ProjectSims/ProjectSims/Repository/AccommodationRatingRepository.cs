a94b4f9fe7019fb4ee36e8c6aa2dd55e2e615cf7
Jelena <jblanusa13@gmail.com>
2023-04-15 18:02:03 +0200

    Added Rating class, Rating repository, File handler and Service

---
+++ ProjectSims/ProjectSims/Repository/AccommodationRatingRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectSims.Domain.Model;
+using ProjectSims.FileHandler;
+using ProjectSims.WPF.View.Guest1View.Pages;
+
+namespace ProjectSims.Repository
+{
+    public class AccommodationRatingRepository
+    {
+        private AccommodationRatingFileHandler ratingFileHandler;
+        private List<AccommodationAndOwnerRating> ratings;
+
+        public AccommodationRatingRepository()
+        {
+            ratingFileHandler = new AccommodationRatingFileHandler();
+            ratings = ratingFileHandler.Load();
+        }
+
+        public List<AccommodationAndOwnerRating> GetAll()
+        {
+            return ratings;
+        }
+
+        public AccommodationAndOwnerRating Get(int id)
+        {
+            return ratings.Find(r => r.Id == id);
+        }
+
+        public void Add(AccommodationAndOwnerRating rating)
+        {
+            ratings.Add(rating);
+            ratingFileHandler.Save(ratings);
+        }
+    }
+}
---
+++ ProjectSims/ProjectSims/Service/AccommodationRatingService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectSims.Domain.Model;
+using ProjectSims.Repository;
+using ProjectSims.WPF.View.Guest1View.Pages;
+
+namespace ProjectSims.Service
+{
+    public class AccommodationRatingService
+    {
+        private AccommodationRatingRepository ratingRepository;
+        private List<AccommodationAndOwnerRating> ratings;
+
+        public AccommodationRatingService()
+        {
+            ratingRepository = new AccommodationRatingRepository();
+            ratings = ratingRepository.GetAll();
+        }
+
+        public AccommodationAndOwnerRating GetRating(int id)
+        {
+            return ratingRepository.Get(id);
+        }
+
+
+        public int NextId()
+        {
+            return ratings.Max(r => r.Id) + 1;
+        }
+
+    }
+}
