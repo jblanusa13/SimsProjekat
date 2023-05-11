229e955231fa88826491d57f56d99ce130e76727
Jovan Sarac <jovan_bn@outlook.com>
2023-05-11 12:07:38 +0200

    Added page AccountView in Guest2View

---
+++ ProjectSims/ProjectSims/WPF/View/Guest2View/Pages/AccountView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ProjectSims.Domain.Model;
+using ProjectSims.FileHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectSims.WPF.View.Guest2View.Pages
+{
+    /// <summary>
+    /// Interaction logic for AccountView.xaml
+    /// </summary>
+    public partial class AccountView : Page
+    {
+        public string UserNameAndSurname { get; set; }
+        public string AccountUsername { get; set; }
+        private UserFileHandler userFile;
+        public Guest2 guest2 { get; set; }
+        public AccountView(Guest2 g)
+        {
+            InitializeComponent();
+            DataContext = this;
+            guest2 = g;
+            UserNameAndSurname = g.Name + " " + g.Surname;
+            userFile = new UserFileHandler();
+            AccountUsername = userFile.Get(guest2.UserId).Username;
+        }
+    }
+}
