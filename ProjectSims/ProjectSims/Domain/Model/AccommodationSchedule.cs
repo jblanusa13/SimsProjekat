c273ff23d2ca0fcc204116b31bf952adb79196b1
Jelena <jblanusa13@gmail.com>
2023-05-12 11:16:55 +0200

    Made AccommodationSchedule.cs

---
+++ ProjectSims/ProjectSims/Domain/Model/AccommodationSchedule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSims.Domain.Model
+{
+    public class AccommodationSchedule
+    {
+        public int Id { get; set; }
+        public List<DateRanges> UnavailableDates { get; set; }
+
+        public AccommodationSchedule()
+        {
+            UnavailableDates = new List<DateRanges>();
+        }
+
+        public AccommodationSchedule(int id, List<DateRanges> unavailableDates)
+        {
+            Id = id;
+            UnavailableDates = unavailableDates;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            List<DateRanges> unavailableDates = new List<DateRanges>();
+            foreach (string value in values[1].Split(","))
+            {
+                unavailableDates.Add(new DateRanges(DateOnly.ParseExact(value.Split("-")[0], "dd.MM.yyyy"), DateOnly.ParseExact(value.Split("-")[1], "dd.MM.yyyy")));
+            }
+            UnavailableDates = unavailableDates;
+        }
+
+        public string[] ToCSV()
+        {
+            string UnavailableDatesString = "";
+            foreach (DateRanges range in UnavailableDates)
+            {
+                if (range != UnavailableDates.Last())
+                {
+                    UnavailableDatesString += range.CheckIn.ToString() + "-" + range.CheckOut.ToString() + ",";
+                }
+            }
+            UnavailableDatesString += UnavailableDates.Last().CheckIn + "-" + UnavailableDates.Last().CheckOut;
+            string[] csvValues = {
+                Id.ToString(),
+                UnavailableDatesString
+            };
+            return csvValues;
+        }
+    }
+}
